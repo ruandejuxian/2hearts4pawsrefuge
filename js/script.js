8fce6aa072f494d54eb0f6abaa550de72ec5229c
ruandejuxian <ruanyujian@set.edu.vn>
2025-12-22 21:29:30 +0700

    frist commit

---
+++ js/script.js
@@ -0,0 +1,39 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("carousel-slide");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
+}
+
+// Auto-advance the carousel
+function autoSlides() {
+    let i;
+    let slides = document.getElementsByClassName("carousel-slide");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}
+    slides[slideIndex-1].style.display = "block";
+    setTimeout(autoSlides, 5000); // Change image every 5 seconds
+}
+
+// Start the auto-advance after the initial manual display
+setTimeout(autoSlides, 5000);
